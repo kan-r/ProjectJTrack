4556468a1c015163e6f4b479f65555a0bb92111b
Kan Ranganathan <krangan100@gmail.com>
2020-12-21 08:17:09 +1100

    tidied up

--- pcc-web/src/config/config.js
+++
@@ -1,4 +0,0 @@
-
-export const config = {
-    apiURL: 'http://localhost:8082'
-};
--- pccREST/src/main/java/com/kan/dao/PurchaseDao.java
+++
@@ -1,9 +0,0 @@
-package com.kan.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kan.model.Purchase;
-
-public interface PurchaseDao extends JpaRepository<Purchase, Integer> {
-
-}
