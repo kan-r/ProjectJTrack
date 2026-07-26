af0087f466b516581e23a83ed8a694b7a62666c9
Kan <krangan100@gmail.com>
2026-07-26 10:56:57 +1000

    Initial commit - jtrack

---
+++ jtrack/src/main/java/com/kan/jtrack/JtrackApplication.java
@@ -0,0 +1,13 @@
+package com.kan.jtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtrackApplication.class, args);
+	}
+
+}
---
+++ jtrack/src/test/java/com/kan/jtrack/JtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kan.jtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
